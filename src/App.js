23538e3cf950b703e03cc2b8d313767662d20bbc
nowjin <hyun0524e@naver.com>
2023-08-15 00:47:44 +0900

    update page design

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
-import logo from './logo.svg';
+import heart from './image/heart.svg';
+import comet from './image/comet.svg';
+import computer from './image/computer.svg';
+import folder from './image/folder.svg';
+import rabbit from './image/rabbit.svg';
+import pressStart from './image/press_start.svg';
+import instagram from './image/instagram.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="container">
-      <a
-        className="link"
-        href="https://reactjs.org"
-        target="_blank"
-      >
-        <img src={logo} className='logo'/>
+      <a href="https://reactjs.org">
+        <img src={heart} className='heart'/>
+      </a>
+      <a href="https://reactjs.org">
+        <img src={comet} className='comet'/>
+      </a>
+      <a href="https://reactjs.org">
+        <img src={computer} className='computer'/>
+      </a>
+      <a href="https://reactjs.org">
+        <img src={folder} className='folder'/>
+      </a>
+      <a href="https://reactjs.org">
+        <img src={rabbit} className='rabbit'/>
+      </a>
+      <a href="https://reactjs.org">
+        <img src={pressStart} className='press-start'/>
+      </a>
+      <a href="https://reactjs.org">
+        <img src={instagram} className='instagram'/>
       </a>
     </div>
   );
