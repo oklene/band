b7583ddaa1ea7f01d29da80820813dc4c6d7c83f
nowjin <hyun0524e@naver.com>
2023-08-15 02:18:05 +0900

    add public url based for browser router

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <div className="container">
         <Routes>
           <Route path="/" element={<Home />} />
